32753a85e493a7b402026d1589ac8f0007c06853
Richard Mohammed <richardm@winningmoves.com>
2013-09-01 12:48:06 +0100

    Point where it nearlty works

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <script src="http://js.pusher.com/2.1/pusher.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+    // Enable pusher logging - don't include this in production
+    Pusher.log = function(message) {
+      if (window.console && window.console.log) {
+        window.console.log(message);
+      }
+    };
+
+    var pusher = new Pusher('504be0f12afa28255aa5');
+    var channel = pusher.subscribe('test_channel');
+    channel.bind('my_event', function(data) {
+      
+      setPosition(data.x, data.y);
+      
+    });
+  </script>
+    </head>
+    <body>
+        
+        <?php ?>
+
+        <div id="control-area">
+            <div id="cursor"></div>
+            <?php 
+$url = 'http://bbc.co.uk';
+
+$html = file_get_contents($url);
+
+echo $html;
+?>
+
+            <!--iframe src="http://bugpurge.com" style="width: 100%; height: 1000px;"></iframe-->
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
---
+++ server.js
@@ -0,0 +1,70 @@
+var http = require('http');
+
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '53117',
+  key: '504be0f12afa28255aa5',
+  secret: 'ec99e461803130e71e98'
+});
+
+
+function setPosition(response, x, y)
+{
+    
+    pusher.trigger('test_channel', 'my_event', {
+      "x": x,
+      "y": y
+    });
+    
+    response.write('<h1>Moving cursor to (x='+x+'px, y='+y+'px)</h1>');
+}
+
+http.createServer(function (req, response) {
+    
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  
+  
+  
+  http.get("http://www.google.com/index.html", function(res) {
+      console.log(req.url);
+      
+      var queryString = req.url;
+      
+      var left = queryString.split('/?');
+      
+      queryString = left[1];
+      
+      if(queryString)
+      {
+      
+      queryString = queryString.split('&');
+      
+      var x = queryString[0];
+      var y = queryString[1];
+      
+      console.log('queryStringSTART');
+      console.log(queryString);
+      console.log('queryStringEND');
+        x = x.split('=');
+        y = y.split('=');
+      
+      x = x[1]; 
+      y = y[1]; 
+      
+      setPosition(response, x, y);
+
+
+        
+      }
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+  
+  response.end('Yay!');
+  
+}).listen(1337, '127.0.0.1');
+
+
+
+console.log('Server running at http://127.0.0.1:1337/');
